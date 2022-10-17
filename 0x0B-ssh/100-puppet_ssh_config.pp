23644e3690f9978d5a64a7384293ee5eacfedeeb
mmoh-i <ibrahimmusty100@yahoo.com>
2022-10-17 04:19:15 -0700

    Automating ssh with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#using Puppet to make changes to our config file to connect to a srver without using password
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '  IdentifyFile ~/.ssh/school',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '  PasswordAuthentication no',
+  replace => true,
